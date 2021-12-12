e2d021b09e8c44180a1907e71377048f3904b036
alex <desorbaix@free.fr>
2021-12-12 19:51:36 +0100

    tp7a

--- projets/tp7a/tp7a.ino
+++ projets/tp7a/tp7a.ino
@@ -1,3 +1,17 @@
+// Include the libraries we need
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 5
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+
 //librairie i2C
 #include <Wire.h>
 //structure de gestion de temps
@@ -14,11 +28,31 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
   Serial.begin(9600);
+    // Start up the library
+  sensors.begin();
   lcd.begin(16, 2);
-  lcd.println("DS1307RTC Read Test");
+  lcd.println("DS1307RTC");
 }
 
 void loop() {
+  lcd.setCursor(0,9);
+  lcd.print("      ");
+  lcd.setCursor(0,9);
+
+  sensors.requestTemperatures(); // Send the command to get temperatures
+   // After we got the temperatures, we can print them here.
+  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+  float tempC = sensors.getTempCByIndex(0);
+
+  // Check if reading was successful
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+    lcd.println(tempC);
+  } 
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
   tmElements_t tm;
   lcd.setCursor(1,0);
   lcd.print("                ");
